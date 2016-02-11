4d4e3b0d7ef86a0d1cffd1b245590c691b349081
andreymyster <and001@gmail.com>
2016-02-12 01:46:02 +0300

    Add cart get numbers of items

--- public/app.js
+++ public/app.js
@@ -1,16 +1,30 @@
-
 function something()
 {
     var x = window.localStorage.getItem('aaa');
-
     x = x * 1 + 1;
-
     window.localStorage.setItem('aaa', x);
-
     alert(x);
 }
 
 function add_to_cart(id)
 {
-    alert('Вы заказали пиццу под номером ' + id);
+    var key = 'product_' + id;
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
+}
+
+function count_items()
+{
+    var sum = 0;
+    for (var i = 0, len = localStorage.length; i < len; i++)
+    {
+        var key = localStorage.key(i);
+        if (key.indexOf("product_") == 0)    // функция indexOf возвращает номер в строке с которого начинается искомое сочетание
+        {
+            var value = localStorage[key] * 1;
+            sum = sum + value;
+        }
+    }
+    return sum;
 }
